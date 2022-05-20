9e8b8d5885acd390ed5d9fe87cd14c9be8bbefbd
Patrick Graham <patrick.j.m.graham@googlemail.com>
2022-05-20 16:15:45 +0200

    done the views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get 'lists', to: 'lists#index'
+  get 'lists/:id', to: 'lists#show'
+  get 'lists/new', to: 'lists#new'
+  post 'lists', to: 'lists#create'
+  get 'lists/:id/bookmarks', to: 'bookmarks#index'
+  get 'lists/:id/bookmarks/new', to: 'bookmarks#new'
+  post 'lists/:id/bookmarks', to: 'bookmarks#create'
+  delete 'lists/:id/bookmarks/:id', to: 'bookmarks#destroy'
 end
